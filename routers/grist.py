06ccc57b0703a2e3dbc3d6e563fa8b296ef67610
Ben Sanders <ben.sanders97@gmail.com>
2026-08-15 22:29:04 -0400

    Removed ag-grid.

---
+++ routers/grist.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from services import update_grist_service
+
+router = APIRouter()
+
+@router.post("/grist")
+def update_grist():
+    update_grist_service()
+    return
--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,4 @@
 from .transaction_services import sync_transactions
+from .grist import update_grist_service
 
-__all__ = ["sync_transactions"]
+__all__ = ["sync_transactions", "update_grist_service"]
